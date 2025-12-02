eeff3b3bbd4cbbe91f1bb8f2b27c8ba78b5e37a3
Julian Federico Viel <111827169+Eilifir@users.noreply.github.com>
2025-12-01 23:48:51 -0300

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //---------------------References---------------------
+    Rigidbody2D rb;
+    //---------------------Variables---------------------
+    Vector2 input;
+    [SerializeField] float moveSpeed = 5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        input = new Vector2(horizontal, vertical);
+        input = input.normalized;
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = input * moveSpeed;
+    }
+}
