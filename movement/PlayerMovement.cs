473150a3954c063565e68970a91c54af62ac1217
Julian Federico Viel <111827169+Eilifir@users.noreply.github.com>
2025-12-03 11:07:16 -0300

    Update PlayerMovement.cs

--- movement/PlayerMovement.cs
+++ movement/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     //---------------------Variables---------------------
     Vector2 input;
-    [SerializedField] float moveSpeed = 5f;
+    [SerializedField] float moveSpeed = 5f;    //If you're using the modular sprint/run script change this into public instead of [SerializedFiels]
 
     void Start()
     {
@@ -19,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         input = new Vector2(horizontal, vertical);
-        input = input.normalized;
+        input = input.normalized;    //Normalize the vector so you move at the same speed in every direction.
     }
     private void FixedUpdate()
     {
